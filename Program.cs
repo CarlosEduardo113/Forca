0c573ca6cc14a5de3a3b4ea769bea3c04ec41ace
CarlosEduardo113 <ce219560@gmail.com>
2025-03-29 20:54:12 -0300

    Finalizar

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+        // Forca - Exercício 3 da Lista 2
+
+        Console.WriteLine("\n\n\n-------------");
+        Console.WriteLine("\n\n\n--- Forca ---");
+        Console.WriteLine("\n\n\n-------------");
+
+
+        Console.Write("\nQual a palavra secreta? ");
+        string palavraSecreta = Console.ReadLine().ToLower(); 
+
+        Console.Write("Qual a letra? ");
+        char letra = Char.ToLower(Console.ReadLine()[0]); 
+
+
+        bool existeNaPalavra = palavraSecreta.Contains(letra.ToString());
+
+
+        Console.WriteLine($"A letra \"{letra}\" existe na palavra secreta => {existeNaPalavra}");
